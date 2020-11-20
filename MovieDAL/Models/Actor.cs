723a620d1ebb1d55cce6709cd2e51b4b73bfe783
vox uni <contact@aftercoaching.be>
2020-11-20 15:30:06 +0100

    models Actor ok

--- MovieDAL/Models/Actor.cs
+++ MovieDAL/Models/Actor.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace DAL.Models
 {
-    class Actor
+    public class Actor : IEntity<int>
     {
+
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public Actor(string lastName, string firstName)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+        }
+
+        public Actor(int id, string lastName, string firstName) : this (lastName, firstName)
+        {
+            Id = id;
+
+        }
+
+        public Actor()
+        {
+
+        }
     }
 }
